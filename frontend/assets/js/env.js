413e7210400ad22d9b4b95b8eb5173af111e8d0b
FilbertNg <filbertng1303@gmail.com>
2025-12-19 14:46:47 +0800

    The latest

--- frontend/assets/js/env.js
+++ frontend/assets/js/env.js
@@ -4,4 +4,5 @@
 // This file sets the configuration for when you are running the site locally.
 // On Netlify, this file will be created automatically during the build process.
 
-AppConfig.BASE_URL = "https://incredible-respect-f1309c941e.strapiapp.com/";
+AppConfig.BASE_URL = "http://localhost:1337";
+// AppConfig.BASE_URL = "https://incredible-respect-f1309c941e.strapiapp.com";
